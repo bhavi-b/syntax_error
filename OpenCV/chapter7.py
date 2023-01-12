aed974091ba7e94df2e3b300c1f5fe00607ce610
bhavi-b <bhavishaybharti@gmail.com>
2023-01-13 05:00:08 +0530

    just playin around

--- OpenCV/chapter7.py
+++ OpenCV/chapter7.py
@@ -38,7 +38,7 @@ def empty(a):
     pass
 
 
-path = "Resources/lambo.png"
+# path = "Resources/lambo.png"
 cv2.namedWindow('Trackbars')
 cv2.resizeWindow('Trackbars', 640, 240)
 cv2.createTrackbar('Hue Min', 'Trackbars', 0, 179, empty)
@@ -49,7 +49,7 @@ cv2.createTrackbar('Val Min', 'Trackbars', 153, 255, empty)
 cv2.createTrackbar('Val Max', 'Trackbars', 255, 255, empty)
 
 while True:
-    img = cv2.imread(path)
+    img = cv2.imread("D:\SDS\syntax_error\OpenCV\Resources\lambo.png")   #specify complete path in windows
     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     h_min = cv2.getTrackbarPos('Hue Min', 'Trackbars')
     h_max = cv2.getTrackbarPos('Hue Max', 'Trackbars')
